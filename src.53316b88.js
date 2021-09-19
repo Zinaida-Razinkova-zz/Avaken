56c25667846213a43c59aa3cee5b678305f54877
Zinaida-Razinkova <68029159+Zinaida-Razinkova@users.noreply.github.com>
2021-09-19 07:30:36 +0000

    Deploying to gh-pages from @ Zinaida-Razinkova/Avaken@cac05624dd4095d9cbba28b3e14ca19f338477df 🚀

--- src.53316b88.js
+++ src.53316b88.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Avaken/src.e0d31abf.js.map
+//# sourceMappingURL=/Avaken/src.53316b88.js.map
